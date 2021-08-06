1b57d36fd19102fc72a5591a5eaaeb80f307c0ca
AKASHKASAR <akash.kasar20798@gmail.com>
2021-08-06 15:58:22 +0530

    Commit

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route, Link, BrowserRouter } from "react-router-dom";
 
-const Header = () => (
+const Header = () => (// header
   <div>
     <nav className="navbar navbar-expand-md navbar-dark bg-dark">
       <Link className="navbar-brand" to="/home">
